aec3a23a8f36ba232e5f229b89c77408989ccc08
ozerbakar <71891704+ozerbakar@users.noreply.github.com>
2025-08-24 14:55:49 +0300

    Update and rename ReadData.R to ReadData_LoadPackages.R

--- ReadData_LoadPackages.R
+++ ReadData_LoadPackages.R
@@ -1,12 +1,28 @@
+## Loading necessary R packages
+
+# To deal and analyse with demographic data
+lapply(c("demography", "StMoMo", "HMDHFDplus"), require, character.only = TRUE)
+
+# To manipulate data
+lapply(c("dplyr"), require, character.only = TRUE)
+
+# To perform machine learning techniques and calculate errors
+lapply(c("mlr", "MLmetrics", "stringr"), require, character.only = TRUE)
+
+# To show and save error values in a table format
+lapply("data.table", require, character.only = TRUE)
+
+# To make several graphs
+lapply(c("ggplot2", "patchwork"), require, character.only = TRUE)
+
+# To prevent scientific numbers notation
 options(scipen = 999)
 
 # Reading the Data
+DNK <- hmd.mx(country = "DNK", username = "ozerbakar@hacettepe.edu.tr", 
+              password = "Ozer19871905*", label = "DENMARK")
 
-library(demography)
-DNK <- hmd.mx(country = "DNK", username = "your username", 
-              password = "your password", label = "DENMARK")
-
-library(StMoMo)
+# Transforming demography data into "StMoMo" data.
 DNK.Fem <- StMoMoData(DNK, series = "female", type = "central")
 DNK.Male <- StMoMoData(DNK, series = "male", type = "central")
 
@@ -24,7 +40,6 @@ DNK.Male.Bin$Dxt <- round(DNK.Male.Bin$Dxt)
 # These numbers are slightly different with the observed number of deaths. We can read
 # the exact number of deaths with the package HMDHFDplus.
 
-library(HMDHFDplus)
 N.of.Deaths <- readHMDweb("DNK", "Deaths_1x1", "ozerbakar@hacettepe.edu.tr", 
                           "Ozer19871905*", fixup = TRUE)
 
@@ -35,7 +50,7 @@ RH <- rh(link = "log", cohortAgeFun = "1")
 APC <- apc(link = "log")
 M7 <- m7(link = "logit")
 
-# Pre-defined Plat function is not included in StMoMo package
+# Pre-defined Plat function is not included in "StMoMo" package
 f.Plat <- function(x, ages) mean(ages) - x
 constPlat <- function(ax, bx, kt, b0x, gc, wxt, ages){
   nYears <- dim(wxt)[2]
@@ -60,4 +75,3 @@ Plat <- StMoMo(link = "log", staticAgeFun = TRUE,
                periodAgeFun = c("1", f.Plat), 
                cohortAgeFun = "1", 
                constFun = constPlat)
-
