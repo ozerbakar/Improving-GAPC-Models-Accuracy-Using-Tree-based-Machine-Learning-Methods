83ddeeec90652fe9abee705bd842bbd7769ea4d4
ozerbakar <71891704+ozerbakar@users.noreply.github.com>
2025-09-06 22:07:46 +0300

    Update Figure_Train-Test.R

--- Figure_Train-Test.R
+++ Figure_Train-Test.R
@@ -1,11 +1,11 @@
 
-min_train <- graph.RF_CBD.fem %>%
+min_train <- graph.GB_LC.fem %>%
   filter(rmse.train == min(rmse.train))
 
-train_plot <- ggplot(graph.RF_CBD.fem, aes(x = Hyper.p, y = rmse.train)) +
+train_plot <- ggplot(graph.GB_LC.fem, aes(x = Hyper.p, y = rmse.train)) +
   geom_point(color = "blue", size = 3) +
-  geom_point(data = min_train, color = "darkgreen", size = 3, shape = 17, fill = "green", stroke = 2) + 
-  geom_text(data = min_train, aes(label = round(rmse.train, 4)), vjust = -1, hjust = 0.5, color = "darkgreen", size = 3) + 
+  geom_point(data = min_train, color = "darkgreen", size = 3, shape = 2, stroke = 2) + 
+  geom_text(data = min_train, aes(label = round(rmse.train, 4)), vjust = -2, hjust = 0.5, color = "darkgreen", size = 3) + 
   labs(
     title = "TRAIN",
     x = "Hyperparameter Set",
@@ -13,17 +13,18 @@ train_plot <- ggplot(graph.RF_CBD.fem, aes(x = Hyper.p, y = rmse.train)) +
   ) +
   theme_minimal() +
   theme(
-    axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 10),
+    axis.text.y = element_text(size = 10),
     plot.title = element_text(hjust = 0.5, face = "bold")
   )
 
-min_test <- graph.RF_CBD.fem %>%
+min_test <- graph.GB_LC.fem %>%
   filter(rmse.test == min(rmse.test))
 
-test_plot <- ggplot(graph.RF_CBD.fem, aes(x = Hyper.p, y = rmse.test)) +
+test_plot <- ggplot(graph.GB_LC.fem, aes(x = Hyper.p, y = rmse.test)) +
   geom_point(color = "red", size = 3) +
-  geom_point(data = min_test, color = "darkgreen", size = 3, shape = 17, fill = "green", stroke = 2) + 
-  geom_text(data = min_test, aes(label = round(rmse.test, 4)), vjust = -1, hjust = 0.5, color = "darkgreen", size = 3) +
+  geom_point(data = min_test, color = "darkgreen", size = 3, shape = 2, stroke = 2) + 
+  geom_text(data = min_test, aes(label = round(rmse.test, 4)), vjust = -2, hjust = 0.5, color = "darkgreen", size = 3) +
   labs(
     title = "TEST",
     x = "Hyperparameter Set",
@@ -31,10 +32,16 @@ test_plot <- ggplot(graph.RF_CBD.fem, aes(x = Hyper.p, y = rmse.test)) +
   ) +
   theme_minimal() +
   theme(
-    axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 10),
+    axis.text.y = element_text(size = 10),
     plot.title = element_text(hjust = 0.5, face = "bold")
   )
 
-train_plot + test_plot
-
+ttplot.SWE <- train_plot + test_plot +
+  plot_annotation(
+    title = "GB-LC SWEDEN",
+    theme = theme(
+      plot.title = element_text(hjust = 0.5, face = "bold", size = 16))
+  )
+ggsave("SWE.TrainTest.plot.png", plot = ttplot.SWE, width = 8, height = 6, dpi = 600)
 
